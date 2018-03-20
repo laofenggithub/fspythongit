5b23d905c4840c6cc6428a5788d7bed81262e5d7
丰帅 <fengshuai@fengshuaideMacBook-Pro.local>
2018-03-20 21:46:11 +0800

    第一课练习

--- TempConvert.py
+++ TempConvert.py
@@ -1,5 +1,5 @@
-#TempConvert.py
-TempStr = input("please input temperature value: ")
+# TempConvert.py
+'''TempStr = input("please input temperature value: ")
 if TempStr[-1] in ['F', 'f']:
     C = (eval(TempStr[0:-1]) - 32) / 1.8
     print("After convert value is {:.2f}C".format(C))
@@ -12,3 +12,31 @@ else:
 
 print("88"*8)
 print(88*8)
+
+TempStr = input()
+if TempStr[0:3] in ['RMB']:
+    RMB = eval(TempStr[3:]) / 6.78
+    print("USD{:.2f}".format(RMB))
+elif TempStr[0:3] in ['USD']:
+    USD = eval(TempStr[3:]) * 6.78
+    print("RMB{:.2f}".format(USD))
+
+TempStr = input()
+if TempStr[-1] in ['m']:
+    _in = eval(TempStr[0:-1]) *39.37
+    print("{:.3f}in".format(_in))
+elif TempStr[-2:] in ['in']:
+    _m = eval(TempStr[0:-2]) /39.37
+    print("{:.3f}m".format(_m))
+    '''
+# turtle.pancolor("black")
+import turtle,time
+turtle.screensize()
+'''
+for i in range(1,5):
+    turtle.circle(10*i)
+'''
+for i in range(9):
+    turtle.forward(100)
+    turtle.left(80)
+time.sleep(3)
